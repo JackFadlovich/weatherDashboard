79f2365e5f25076398ba8129123accddd0340d27
Jack <108418371+ZolarEclipse@users.noreply.github.com>
2024-08-21 11:30:25 -0500

    added dependencies

---
+++ assets/js/script.js
@@ -0,0 +1,7 @@
+const apiKey = '34135c0aeed98d4a373858b72310f84d';
+
+const form = document.getElementById('city-search-form');
+const cityInput = document.getElementById('city-input');
+const searchHistoryContainer = document.getElementById('search-history');
+const currentDetails = document.getElementById('current-details');
+const forecastDetails = document.getElementById('forecast-details');
